53e698c1424c179954ef1178c9016dcf8b052d7c
BONNI VR <bonni_pr13@outlook.com>
2025-02-05 20:14:35 -0600

    HICE EL EJERCIO 4

---
+++ src/js/Script1.js
@@ -0,0 +1,10 @@
+// Declaración de variables
+let x = 10;
+let y = 5;
+let z = 2;
+
+// Cálculo
+let resultado = (x + y) * z / 2;
+
+// Imprimir el resultado en la consola
+console.log("El resultado es:", resultado);
